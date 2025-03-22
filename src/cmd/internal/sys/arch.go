07ec01ffb96fa078d4ecbea0d22b375d3872a096
Jason E. Aten <j.e.aten@gmail.com>
2025-03-21 20:08:37 -0500

    cmd/internal/sys/arch.go add ArchWasm32 no idea if correct, total guess

--- src/cmd/internal/sys/arch.go
+++ src/cmd/internal/sys/arch.go
@@ -268,6 +268,19 @@ var ArchWasm = &Arch{
 	FixedFrameSize: 0,
 }
 
+var ArchWasm32 = &Arch{
+	Name:           "wasm32",
+	Family:         Wasm,
+	ByteOrder:      binary.LittleEndian,
+	PtrSize:        8,
+	RegSize:        8,
+	MinLC:          1,
+	Alignment:      1,
+	CanMergeLoads:  false,
+	HasLR:          false,
+	FixedFrameSize: 0,
+}
+
 var Archs = [...]*Arch{
 	Arch386,
 	ArchAMD64,
@@ -283,4 +296,5 @@ var Archs = [...]*Arch{
 	ArchRISCV64,
 	ArchS390X,
 	ArchWasm,
+	ArchWasm32,
 }
